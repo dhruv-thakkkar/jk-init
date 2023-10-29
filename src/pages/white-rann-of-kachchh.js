ffb4b3c79a14ca7e3bbfa499f27a5ab9a0650088
unknown <dnt1997@gmail.com>
2023-10-29 14:12:23 +0530

    adding rann of kachchh

--- src/pages/white-rann-of-kachchh.js
+++ src/pages/white-rann-of-kachchh.js
@@ -1,22 +1,4 @@
 import React from "react";
-import SimpleImageSlider from "react-simple-image-slider";
-const images = [
-  {
-    url:
-      window.location.origin +
-      "/assets/images/rann-of-kachchh/rann-of-kachchh-jk-1.jpg",
-  },
-  {
-    url:
-      window.location.origin +
-      "/assets/images/rann-of-kachchh/rann-of-kachchh-jk-2.jpg",
-  },
-  {
-    url:
-      window.location.origin +
-      "/assets/images/rann-of-kachchh/rann-of-kachchh-jk-3.jpg",
-  },
-];
 function Kachchh() {
   return (
     <div>
