41582d1b264121e3632462105fd16f723540a1cc
unknown <dnt1997@gmail.com>
2023-10-29 14:01:09 +0530

    adding rann of kachchh

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       </div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="white" element={<Kachchh />} />
+        <Route path="white-rann-of-kachchh" element={<Kachchh />} />
       </Routes>
       <Footer />
     </BrowserRouter>
