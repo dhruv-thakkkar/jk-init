a1251b3b5516608449957334c8e1998e9966b464
unknown <dnt1997@gmail.com>
2023-10-29 13:58:30 +0530

    adding rann of kachchh

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom' 
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import Kachchh from './pages/white-rann-of-kachchh';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Kachchh from "./pages/white-rann-of-kachchh";
 function App() {
   return (
-    <BrowserRouter >
-    <div className="App">
-    <Navbar />
-    </div>
-    <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="white-rann-of-kachchh" element={<Kachchh />} />
-    </Routes>
-    <Footer />
-
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="white" element={<Kachchh />} />
+      </Routes>
+      <Footer />
     </BrowserRouter>
-   
-  )
+  );
 }
 
 export default App;
